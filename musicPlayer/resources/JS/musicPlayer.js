2db18449230f7512ccd85519160016ee5519ea95
yooeuddeum <145522980+yooeuddeum@users.noreply.github.com>
2023-12-08 17:10:08 +0900

    20231208 오후수업

---
+++ musicPlayer/resources/JS/musicPlayer.js
@@ -0,0 +1,23 @@
+const menuItems = document.querySelectorAll(".menu-item");
+// console.log(menuItems);
+menuItems.forEach(function(el, idx){
+    el.addEventListener('click', function(){
+        if(idx == 0){
+            location.href = 'index.html';
+        }
+    });
+});
+
+const wraps = document.querySelectorAll("header .wrap > span");
+wraps.forEach(function(el, idx){
+    el.addEventListener('click', function(){
+        if(idx == 0){
+            location.href = 'musicPlayer.html'
+        };
+    });
+});
+
+            
+          
+
+
