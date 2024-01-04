acb0fb704d3fcd78d924f296559fe1a01e3899ee
yooeuddeum <145522980+yooeuddeum@users.noreply.github.com>
2024-01-04 17:32:38 +0900

    asdasdasd

--- 20240102/js/main.js
+++ 20240102/js/main.js
@@ -1,4 +1,21 @@
 
+
+function createSnow() {
+  const el = document.createElement("div");
+  el.classList.add('snow');
+  el.style.marginLeft = randomPostion() + 'px';
+  document.body.appendChild(el);
+}
+
+function randomPostion() {
+  return Math.floor(Math.random() * window.innerWidth);
+};
+for(let i = 0; i < 300; i++){
+  createSnow();
+}
+createSnow();
+
+  
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
